1ba2fb3cab286b661e04fba7a04748f43da9f9f5
LMorales41 <luis.ssbmorales@gmail.com>
2024-03-26 07:22:41 -0700

    added some function descriptions

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from src.setup_cleaning_funcs import *
 from datascience import *
 import numpy as np
 
+"""DATA SETUP"""
+
 
 #Set up all tables properly
 types_table, pokemon_types, pkmn, effective_chart, moves, learnable_moves = setup_tables()
 
 
 # Shortened code, placed ugly messy non-looksmaxxing code in setup_cleaning_funcs
+# Cleaned up tables by changing names, adding/removing columns; overall made data more workable-with
 pkmn_clean = setup_pkmn_table(pkmn, pokemon_types)
 effective_chart = setup_effective_chart(effective_chart)
 moves = setup_moves_table(moves)
--- src/type_functions.py
+++ src/type_functions.py
@@ -14,14 +14,18 @@ PKMN_TYPES = [
 
 
 def remove_and_sort(arr):
+    """Grabs an array with potentially multiple of same element and sorts + removes for unique elements in this array"""
+    # Uses a set
     arr = sorted(set(arr))
     return arr
 
 def reformat_move(move):
+    """Grabs a (legal) move and replaces spaces with hyphens to be usable in table; changes to lowercase"""
     reformatted_move = move.replace(' ', '-').lower()
     return reformatted_move
 
 def check_move_legality(move, moves_tbl):
+    """If move exists in table, return True, if move does not exist, return False"""
     if (moves_tbl.where('move_name', are.equal_to(move)).num_rows >= 1):
         return True
     else:
@@ -31,6 +35,7 @@ def check_move_legality(move, moves_tbl):
 
     
 def check_unique_move(moveset, move):
+    """Iterates through moveset, if move shows up more than one time, returns false"""
     for x in moveset:
         if x == move:
             return False
@@ -38,7 +43,7 @@ def check_unique_move(moveset, move):
     return True
 
 def id_into_string(type_id):
-    """I will change an int into a string based on what type it is in the database, using a map/dictionary"""
+    """Changes a singular type_id int into a string based on what type it is in the database, using a map/dictionary"""
     types = {1 : 'normal', 2: 'fighting', 3 : 'flying', 4 : 'poison', 5 : 'ground',
                 6 : 'rock', 7 : 'bug', 8 : 'ghost', 9 : 'steel', 10 : 'fire',
                 11 : 'water', 12 : 'grass', 13 : ' electric', 14 : 'psychic', 15 : 'ice',
@@ -48,7 +53,7 @@ def id_into_string(type_id):
 
 
 def id_into_string(type_ids):
-    """Convert type IDs into strings based on a map"""
+    """Converts multiple (an array of) type IDs into strings based on a map"""
     types = {
         1 : 'normal', 2: 'fighting', 3 : 'flying', 4 : 'poison', 5 : 'ground',
         6 : 'rock', 7 : 'bug', 8 : 'ghost', 9 : 'steel', 10 : 'fire',
@@ -59,22 +64,26 @@ def id_into_string(type_ids):
     return types_string
 
 def multiplier_conversion(multipliers):
-    """I will change an int into a float based on what type it is in the database, using a map/dictionary"""
+    """Changes multiple ints (array of ints) into floats based on what type it is in the database, using a map/dictionary"""
     conversions = {100 : 1.0, 200 : 2.0, 50 : 0.5, 0 : 0.0}
     multipliers_converted = [conversions[num] for num in multipliers]
     return multipliers_converted
 
 def convert_damage_type(moves_col):
-    """I will change the spec/phys to represent the actual damage types"""
+    """Changes damage_id into spec/phys strings to represent the actual damage types; special included it is status moves that do not have effectivenesses, thus excluded for now"""
     conversions = {1 : 'status', 2 : 'physical', 3: 'special' }
     damage_type_converted = [conversions[num] for num in moves_col]
     return damage_type_converted
 
 def remove_none(my_array):
+    """When pokemon is monotype, none is present in type2 so it can fit inside the table. Thus we need to take it out using this function before it causes a mess"""
+    # Array handed to us is always size 2 unless someone (me) does a stinky little oopsie
     filtered_array = [item for item in my_array if 'none' not in item]
     return filtered_array
 
 def type_matchups(multiplier_table, pokemon_types, purpose):
+    """Split into offense or defense so it can check proper column."""
+    # ALWAYS REMOVE NONE or else ruh roh raggy
     remove_none(pokemon_types)
     if (purpose == 'offense'):
         return offensive_matchups(multiplier_table, pokemon_types)
@@ -82,6 +91,8 @@ def type_matchups(multiplier_table, pokemon_types, purpose):
         return defensive_matchups(multiplier_table, pokemon_types)
     
 def offensive_matchups(multiplier_table, pokemon_types):
+    # Same as defensive_matchups, with slight grammatical differences
+    """type_matchups leads here, will return in same order as defensive_matchups. Creates second table to avoid extra commands requiring removing all other types"""
     strong = []
     neutral = []
     weak = []
@@ -110,6 +121,8 @@ def offensive_matchups(multiplier_table, pokemon_types):
 
 
 def defensive_matchups(multiplier_table, pokemon_types):
+    # Same as offensive_matchups, with a few grammatical differences
+    """type_matchups leads here, will return in same order as offensive_matchups. Creates second table to avoid extra commands requiring removing all other types"""
     strong = []
     neutral = []
     weak = []
@@ -134,6 +147,7 @@ def defensive_matchups(multiplier_table, pokemon_types):
     return hit_super_effective_by, neutral, resist_or_immune_to 
 
 def print_effectivenesses(se, neutral, nve):
+    """Prints out what your moves are effective/neutral/ineffective against"""
     print("Your moves are super effective against:", end=' ')
     for x in se:
         print(x, end='')
@@ -156,11 +170,10 @@ def print_effectivenesses(se, neutral, nve):
     print("\n")
 
 
-def stab_effectivenesses(effective_chart, types_to_test, purpose):
-    super_effective, neutral, not_very_effective = type_matchups(effective_chart, types_to_test, purpose)
-    print_effectivenesses(super_effective, neutral, not_very_effective)
-
 def print_resistances(se, neutral, nve):
+    # Does not account for abilities like Water Absorb, Levitate, Dry Skin which change multipliers drastically depending on conditions
+    # Can be further optimized, current output is not accounting for when 2 types multipliers cause something to turn neutral or super effective or immune
+    """Prints what types your Pokemon is weak/neutral/resistant to"""
     print("You are vulnerable to super effective moves from:", end=' ')
     for x in se:
         print(x, end='')
@@ -182,7 +195,14 @@ def print_resistances(se, neutral, nve):
             print(', ', end='')
     print("\n")
 
+
+def stab_effectivenesses(effective_chart, types_to_test, purpose):
+    """Combines type_matchups, offensive_matchups, and print_effectivenesses functions into one clean callable line"""
+    super_effective, neutral, not_very_effective = type_matchups(effective_chart, types_to_test, purpose)
+    print_effectivenesses(super_effective, neutral, not_very_effective)
+
 def resistances(effective_chart, types_to_test, purpose):
+    """Combines type_matchups, defensive_matchups, and print_resistances functions into one clean callable line"""
     super_effective, neutral, not_very_effective = type_matchups(effective_chart, types_to_test, purpose)
     print_resistances(super_effective, neutral, not_very_effective)
 
